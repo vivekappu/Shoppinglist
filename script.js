24a24f08a91b0d5cf48cc3b45c199595936ffb25
vivek <vivek.appu619@gmail.com>
2020-05-03 21:34:31 +0530

    slight changes

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const jsonurl="shoppinglist.json";
-const wikiurl="";
 const btn=document.querySelector("button");
 const table=document.querySelector('table ');
 const select=document.getElementById('filter');
@@ -7,9 +6,9 @@ const span=document.querySelector('span');
 let data;
 //Ajax request to get json file
 function getJson(url,callback){
- const xhr=new XMLHttpRequest();
- xhr.open('GET',url);
- xhr.onload=()=>{
+ const xhr=new XMLHttpRequest();//xhr request 
+ xhr.open('GET',url); 
+ xhr.onload=()=>{   
     if(xhr.status===200){
          data=JSON.parse(xhr.responseText);
         callback(data);
@@ -18,9 +17,12 @@ function getJson(url,callback){
  };
  xhr.send();
 }
-
+/**
+ * generates html required for table using json data
+ * @param  {array} data  json data
+ */
 function generateHTML(data){
-    
+    //function to generate html for table 
     let html=`<thead>`;
     html+=`<tr>`;
     html+=`<th>Serial number</th>`;
@@ -44,25 +46,35 @@ function generateHTML(data){
     }
     );
     html+='</tbody>';
-    table.innerHTML=html;
+    table.innerHTML=html;// adding html generated into tbody
 }
 
 btn.addEventListener('click', (e)=>{
- getJson(jsonurl,generateHTML);
- span.style.display="block";
- e.target.remove();
+ getJson(jsonurl,generateHTML);// getting json data and generating html
+ span.style.display="block";// display select options 
+ e.target.remove();// remove button 
 });
 
 select.addEventListener('change',()=>{
+    /* on changing filter option filter json data 
+    according to type of data*/
     generateHTML(filter(data,select.value));
    
 });
-
-
+/**
+ * filter json data
+ * @param  {array} json first argument
+ * @param  {string} department The second argument
+ * @return {array}   filtered json
+ */
+// function to filter json data 
 function filter(json,department){
+    // filter json if it's all then  just return json data as it is
     if (department.toLowerCase()==="all"){
         return json;
     }
-     return json.filter(i=> i["Department"].toLowerCase()==department.toLowerCase())
+    // otherwise filter according to deparment chosen
+     return json.filter(i=> i["Department"].toLowerCase()==department.toLowerCase());
+    
 }
 
